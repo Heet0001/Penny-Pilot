c2dfb57c48ea4dc160cb6386a379a8653c4ccd58
Heet Ladani <heetladani11@gmail.com>
2025-05-24 22:49:59 +0530

    Changing the server file for renaming the env variables

--- server.js
+++ server.js
@@ -43,10 +43,10 @@ app.use(express.static(path.join(__dirname, "public")))
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 
